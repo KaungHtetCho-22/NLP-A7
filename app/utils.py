82a55fd8d295c7357048025ea7c953d4890f30ff
KaungHtetCho-22 <kaunghtetcho.ait@gmail.com>
2024-03-26 00:08:29 +0700

    update file

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,3 @@
-import cloudpickle
-import torch
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-def load_conversational_chain(chain_path):
-    with open(chain_path, 'rb') as f:
-        chain = cloudpickle.load(f)
-    return chain
-
-def perform_inference(chain, question):
-    return chain({"question": question})
+version https://git-lfs.github.com/spec/v1
+oid sha256:11a987e7fc0e196a9806c6c2182c4f52335c4c4cd77a2c296d19e42874f09d92
+size 319
